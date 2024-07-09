aee655a7750978f8df4df660ed3bc712f6218e19
ionut8891 <148498333+ionut8891@users.noreply.github.com>
2024-07-09 11:15:08 -0400

    Requis 3

--- processor.js
+++ processor.js
@@ -3,8 +3,12 @@ function processor(transmission) {
     if (!transmission.includes("::")) {
         throw new Error('Data is invalid ; should contain "::"');
     }
-    return {};
+    let parts = transmission.split("::");
+    return {
+        id: parts[0]
+    };
 }
+
 
 
 module.exports = processor;
--- processor.test.js
+++ processor.test.js
@@ -10,4 +10,8 @@ describe("transmission processor", function() {
         const expectedError = new Error('Data is invalid ; should contain "::"');
         expect(() => { processor("9701<489584872710>"); }).toThrow(expectedError);
     });
+    test("returns id in object", function () {
+        let result = processor("9701::<489584872710>");
+        expect(result).not.toEqual('id');
+    });
 });
